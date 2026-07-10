2b62c7f6766f6bf443bf56c9e0e1e1927488a5f1
tinwang529 <3365270883@qq.com>
2026-07-10 14:17:59 +0800

    能提交吗

--- commit.bat
+++ commit.bat
@@ -1,21 +1,21 @@
 @echo off
-echo 正在拉取远程最新代码，防止冲突...
+echo ������ȡԶ�����´��룬��ֹ��ͻ...
 git.exe pull --rebase
 
 echo.
-set /p msg=请输入提交信息:
+set /p msg=�������ύ��Ϣ:
 if "%msg%"=="" (
-  echo 提交信息不能为空，脚本结束。
+  echo �ύ��Ϣ����Ϊ�գ��ű�������
   pause
   exit /b
 )
 
-echo 正在添加更改...
+echo �������Ӹ���...
 git.exe add .
 
-echo 正在提交...
+echo �����ύ...
 git.exe commit -m "%msg%"
 
-echo 正在推送代码到当前分支...
+echo �������ʹ��뵽��ǰ��֧...
 git.exe push
 pause
